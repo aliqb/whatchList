aaa8dde4ae027c849071203ea3ac126d601673c4
ali <aliqbhastam@gmail.com>
2020-10-02 15:33:46 +0330

    make list servic and etc

--- src/app/list-item.model.ts
+++ src/app/list-item.model.ts
@@ -1,13 +1,15 @@
 export class ListItem{
-    private poster:string;
-    private title:string;
-    private year:number;
+    public poster:string;
+    public title:string;
+    public year:number;
+    public id:string;
     public desc:string;
     public watched:boolean
-    constructor(poster:string,title:string,year:number,watched:boolean=false){
+    constructor(poster:string,title:string,year:number,id:string,watched:boolean=false){
         this.poster=poster;
         this.title=title;
         this.year=year;
+        this.id=id;
         this.watched=watched;
 
     }
---
+++ src/app/list.service.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@angular/core';
+import { ListItem } from './list-item.model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ListService {
+  items:ListItem[]=[
+    new ListItem(
+      "https://m.media-amazon.com/images/M/MV5BM2MyNjYxNmUtYTAwNi00MTYxLWJmNWYtYzZlODY3ZTk3OTFlXkEyXkFqcGdeQXVyNzkwMjQ5NzM@._V1_SX300.jpg",
+      "The GodFather",
+      1972,
+      "tt0068646",
+    )
+  ]
+  constructor() { }
+  getItems(){
+    return this.items.slice();
+  }
+}
