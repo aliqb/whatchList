5c7c70a1642d044384a44c062f7abe6681510f36
ali <aliqbhastam@gmail.com>
2020-10-02 23:08:56 +0330

    make movieComp btns interactive

--- src/app/movie/movie.component.ts
+++ src/app/movie/movie.component.ts
@@ -1,5 +1,6 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input, OnDestroy, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
+import { Subscription } from 'rxjs';
 import { ListService } from '../list.service';
 import { Movie } from '../movie.model';
 
@@ -8,20 +9,31 @@ import { Movie } from '../movie.model';
   templateUrl: './movie.component.html',
   styleUrls: ['./movie.component.css']
 })
-export class MovieComponent implements OnInit {
-  @Input() movie:Movie;
-  constructor(private router:Router,private rout:ActivatedRoute,private listService:ListService) { }
+export class MovieComponent implements OnInit, OnDestroy {
+  @Input() movie: Movie;
+  added: boolean;
+  addOrRemoveSubs: Subscription;
+  constructor(private router: Router, private rout: ActivatedRoute, private listService: ListService) { }
 
   ngOnInit(): void {
+    this.added = this.listService.hasItem(this.movie.id);
+    this.addOrRemoveSubs = this.listService.addOrDelete.subscribe(() => {
+      this.added = this.listService.hasItem(this.movie.id);
+    })
   }
-  navigate(event){
-    if(!((<HTMLElement>event.target).tagName==='BUTTON')){
+  navigate(event) {
+    if (!((<HTMLElement>event.target).tagName === 'BUTTON')) {
       // console.log('n');
-      this.router.navigate([this.movie.id],{relativeTo:this.rout});
+      this.router.navigate([this.movie.id], { relativeTo: this.rout });
     }
   }
-  addtoList(){
+  addtoList() {
     // console.log("e")
-    this.listService.addItem(this.movie.poster,this.movie.title,this.movie.year,this.movie.id);
+    this.listService.addItem(this.movie.poster, this.movie.title, this.movie.year, this.movie.id);
+  }
+  ngOnDestroy(){
+    if(this.addOrRemoveSubs){
+      this.addOrRemoveSubs.unsubscribe();
+    }
   }
 }
