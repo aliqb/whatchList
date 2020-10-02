50cfdfeb97dce91d6ddd50bd9381e15c684f1b04
ali <aliqbhastam@gmail.com>
2020-10-02 14:59:54 +0330

    fix header routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,11 +4,13 @@ import { AppComponent } from './app.component';
 import { MovieDetailComponent } from './movie-detail/movie-detail.component';
 import { MovieListComponent } from './movie-list/movie-list.component';
 import { MovieComponent } from './movie/movie.component';
+import { WatchListComponent } from './watch-list/watch-list.component';
 
 const routes: Routes = [
   {path:"",redirectTo:'movies',pathMatch:'full'},
   {path:"movies",component:MovieListComponent},
-  {path:"movies/:id",component:MovieDetailComponent}
+  {path:"movies/:id",component:MovieDetailComponent},
+  {path:"watchList",component:WatchListComponent}
 ];
 
 @NgModule({
