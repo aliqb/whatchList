61758651cad9735162e34e0293ba04be2155a7ea
ali <aliqbhastam@gmail.com>
2020-10-03 18:37:12 +0330

    add repeat pass valdator

--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -9,24 +9,34 @@ import { FormControl, FormGroup, Validators } from '@angular/forms';
 export class AuthComponent implements OnInit {
   signUpMode:boolean=true;
   form:FormGroup;
+  fuck="fuck";
+  password:string;
   constructor() { }
 
   ngOnInit(): void {
     this.form=new FormGroup({
       'email':new FormControl('',[Validators.required,Validators.email]),
       'password':new FormControl('',[Validators.required,Validators.minLength(6)]),
-      'passwordRepeat':new FormControl('',[Validators.required,Validators.minLength(6)])
+      'passwordRepeat':new FormControl('',[Validators.required,Validators.minLength(6),this.repPasswordValidator.bind(this)])
 
     })
   }
+  setPassword(){
+    this.password=this.form.controls['password'].value;
+  }
   onSubmit(){
-    console.log(this.form);
+    // console.log(this.form);
+    // console.log(this.form.controls.password.value,this.form.controls.passwordRepeat.value,this.form.controls.password.value===this.form.controls.passwordRepeat.value)
   }
   changeMode(){
     this.signUpMode= !this.signUpMode;
   }
-  // private repPasswordValidator(control:FormControl):{[s:string]:boolean}{
-  //   if(control===this.form.)
-  // }
+  private repPasswordValidator(control:FormControl):{[s:string]:boolean}{
+    // console.log('v',control.value,this.password)
+    if(control.value!==this.password){
+      return {'notSame':true}
+    }
+    return null;
+  }
 
 }
