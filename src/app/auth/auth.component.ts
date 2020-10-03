0db92c8c06c52bdabb0a45efd1c02a0b067c3ce1
ali <aliqbhastam@gmail.com>
2020-10-03 18:59:59 +0330

    make different forms for each mode

--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -7,33 +7,49 @@ import { FormControl, FormGroup, Validators } from '@angular/forms';
   styleUrls: ['./auth.component.css']
 })
 export class AuthComponent implements OnInit {
-  signUpMode:boolean=true;
+  signUpMode:boolean=false;
   form:FormGroup;
   fuck="fuck";
   password:string;
   constructor() { }
 
   ngOnInit(): void {
-    this.form=new FormGroup({
-      'email':new FormControl('',[Validators.required,Validators.email]),
-      'password':new FormControl('',[Validators.required,Validators.minLength(6)]),
-      'passwordRepeat':new FormControl('',[Validators.required,Validators.minLength(6),this.repPasswordValidator.bind(this)])
-
-    })
+    this.makeForm();
   }
   setPassword(){
     this.password=this.form.controls['password'].value;
   }
   onSubmit(){
-    // console.log(this.form);
+    console.log(this.form);
     // console.log(this.form.controls.password.value,this.form.controls.passwordRepeat.value,this.form.controls.password.value===this.form.controls.passwordRepeat.value)
+  }
+  private makeForm(){
+    if(this.signUpMode){
+      this.form=new FormGroup({
+        'email':new FormControl('',[Validators.required,Validators.email]),
+        'password':new FormControl('',[Validators.required,Validators.minLength(6)]),
+        'passwordRepeat':new FormControl('',[this.repPasswordValidator.bind(this)])
+  
+      })
+    }else{
+      this.form=new FormGroup({
+        'email':new FormControl('',[Validators.required,Validators.email]),
+        'password':new FormControl('',[Validators.required,Validators.minLength(6)]),  
+      })
+    }
   }
   changeMode(){
     this.signUpMode= !this.signUpMode;
+    this.makeForm();
+    
   }
   private repPasswordValidator(control:FormControl):{[s:string]:boolean}{
     // console.log('v',control.value,this.password)
-    if(control.value!==this.password){
+    console.log(this.signUpMode)
+    // if(!this.signUpMode){
+    //   return null
+    // }
+    if(control.value!==this.password && this.signUpMode){
       return {'notSame':true}
     }
     return null;
