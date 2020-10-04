fc2f16c4f16b0b06eed657730d7e9f7c98a25726
ali <aliqbhastam@gmail.com>
2020-10-04 14:27:04 +0330

    add auth guard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
+import { AuthGuard } from './auth.guard';
 import { AuthComponent } from './auth/auth.component';
 import { MovieDetailComponent } from './movie-detail/movie-detail.component';
 import { MovieListComponent } from './movie-list/movie-list.component';
@@ -11,7 +12,7 @@ const routes: Routes = [
   {path:"",redirectTo:'movies',pathMatch:'full'},
   {path:"movies",component:MovieListComponent},
   {path:"movies/:id",component:MovieDetailComponent},
-  {path:"watchList",component:WatchListComponent},
+  {path:"watchList",component:WatchListComponent,canActivate:[AuthGuard]},
   {path:'auth',component:AuthComponent}
 ];
 
