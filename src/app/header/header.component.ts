9e9fd8a5607ad529085b45bad5c8df91aea737f4
ali <aliqbhastam@gmail.com>
2020-10-04 12:58:49 +0330

    react header to auth

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,15 +1,24 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { AuthService } from '../auth/auth.service';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css']
 })
-export class HeaderComponent implements OnInit {
-
-  constructor() { }
+export class HeaderComponent implements OnInit,OnDestroy {
+  isAuth:boolean=false;
+  authSubs:Subscription
+  constructor(private authService:AuthService) { }
 
   ngOnInit(): void {
+    this.authSubs=this.authService.user.subscribe(user=>{
+      this.isAuth=!!user;
+    })
+  }
+  ngOnDestroy(){
+    this.authSubs.unsubscribe();
   }
 
 }
