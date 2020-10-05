0f8603adf06f57009a119de19ff38527bb876e9b
ali <aliqbhastam@gmail.com>
2020-10-05 22:16:35 +0330

    fix no year bug

--- src/app/movie-list/movie-list.component.ts
+++ src/app/movie-list/movie-list.component.ts
@@ -36,7 +36,7 @@ export class MovieListComponent implements OnInit {
       // console.log(this.rout.snapshot.queryParams['title']);
       this.currentPage = +this.rout.snapshot.queryParams['page'];
       this.type=this.rout.snapshot.queryParams['type'];
-      console.log('o',this.type);
+      // console.log('o',this.type);
       if(!this.type){
         this.type='all';
       }
