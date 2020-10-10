876dee168c5574166aac2df132207a502a8a9f88
ali <aliqbhastam@gmail.com>
2020-10-10 22:09:21 +0330

    clean dataSErvice

--- src/app/data-storage.service.ts
+++ src/app/data-storage.service.ts
@@ -1,6 +1,5 @@
 import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { switchMap } from 'rxjs/operators';
 import { AuthService } from './auth/auth.service';
 import { ListItem } from './list-item.model';
 import { ListService } from './list.service';
@@ -13,7 +12,6 @@ export class DataStorageService {
 
   constructor(private listService: ListService, private http: HttpClient, private authService: AuthService) { }
   fetchItems(user:User) {
-    console.log("fetch",user);
     if(user){
       this.http.get("https://watchlist-a8e7c.firebaseio.com/list.json",
       { params: new HttpParams().set('auth', user.token) })
