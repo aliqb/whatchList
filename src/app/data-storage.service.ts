caa030ee45be68c829eff4667cb52c10f42fed67
ali <aliqbhastam@gmail.com>
2020-10-10 18:35:07 +0330

    add auth to save method

--- src/app/data-storage.service.ts
+++ src/app/data-storage.service.ts
@@ -1,18 +1,34 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { switchMap } from 'rxjs/operators';
+import { AuthService } from './auth/auth.service';
 import { ListService } from './list.service';
+import { User } from './User.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class DataStorageService {
 
-  constructor(private listService:ListService,private http:HttpClient) { }
-  saveItems(){
-    console.log(this.listService.getItems())
-    this.http.put("https://watchlist-a8e7c.firebaseio.com/list.json",this.listService.getItems())
-    .subscribe(data=>{
-      console.log(data);
+  constructor(private listService: ListService, private http: HttpClient, private authService: AuthService) { }
+  saveItems() {
+    // console.log(this.listService.getItems())
+    // this.http.put("https://watchlist-a8e7c.firebaseio.com/list.json",this.listService.getItems())
+    // .subscribe(data=>{
+    //   console.log(data);
+    // })
+    this.authService.user
+    .pipe(
+      switchMap((user: User) => {
+        if (user) {
+          return this.http.put("https://watchlist-a8e7c.firebaseio.com/list.json",
+           this.listService.getItems(),
+           {params:new HttpParams().set('auth',user.token)})
+        }
+      })
+    )
+    .subscribe(respone=>{
+      console.log(respone)
     })
   }
 }
