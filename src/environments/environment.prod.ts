037a1bf8297377d40d21fee0e7e21461e384905f
Ali Qayyoomi <46604616+aliqb@users.noreply.github.com>
2021-02-08 20:55:52 +0330

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,15 +1,5 @@
 export const environment = {
   production: true,
-  movieApikey:'f04974fd',
-  firebaseApiKey:"AIzaSyCsZTv8pZEgh7pVIEtBWZ_P_IKbQIpV4lk",
-  firebaseConfig : {
-    apiKey: "AIzaSyCsZTv8pZEgh7pVIEtBWZ_P_IKbQIpV4lk",
-    authDomain: "watchlist-a8e7c.firebaseapp.com",
-    databaseURL: "https://watchlist-a8e7c.firebaseio.com",
-    projectId: "watchlist-a8e7c",
-    storageBucket: "watchlist-a8e7c.appspot.com",
-    messagingSenderId: "218253765960",
-    appId: "1:218253765960:web:dce4de03f996b6e7e2177d"
-  }
+ //I have removed my keyd
 
 };
